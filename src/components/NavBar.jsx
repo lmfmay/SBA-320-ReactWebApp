c6930b3d644fb2eb6fb9787447158d84ba64a4bd
lmfmay <176079985+lmfmay@users.noreply.github.com>
2024-11-14 21:04:06 -0800

    Completed NavBar Component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,9 +2,16 @@ import { Link } from "react-router-dom"
 
 function NavBar () {
     return (
-        <nav className="NavBar">
-            <img src="../assets/mocklogo.png" alt="" />
-            <Link to={'/'}>Home</Link>
+        <nav className="navBar">
+            <img src="./src/assets/mocklogo.png" alt="" id="webLogo"/>
+            <h4 id="webName">Echos of Home</h4>
+            <Link to={'/'}>
+            <h4 className="navLinks">Find THE Voice</h4>
+            </Link>
+            <h4 className="navLinks">|</h4>
+            <h4 className="navLinks">Share YOUR Voice</h4> 
+            <button>Login</button>
+            <button>Signup</button>
         </nav>
     )
 }
